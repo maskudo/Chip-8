baba6659f33beea9a2200bc28cb8474e89f4d5df
maskudo <manujkarki101@gmail.com>
2022-07-10 19:57:52 +0545

    test init sdl

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,18 @@
+#define SDL_MAIN_HANDLED
+#include "SDL.h"
 #include "chip8.hpp"
 #include <iostream>
 int main(int argc, char **argv) {
-    chip8::Chip8 interpreter = chip8::Chip8();
-    interpreter.Reset();
-    if (!interpreter.LoadROM(argv[1])) {
-        std::cerr << "Unable to load " << argv[1] << std::endl;
-        return -1;
-    }
-    while (true) {
-        interpreter.Tick();
-    }
+    // chip8::Chip8 interpreter = chip8::Chip8();
+    // interpreter.Reset();
+    // if (!interpreter.LoadROM(argv[1])) {
+    //     std::cerr << "Unable to load " << argv[1] << std::endl;
+    //     return -1;
+    // }
+    // while (true) {
+    //     interpreter.Tick();
+    // }
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Quit();
+    return 0;
 }

4fff2988c1dc1713e03c663b8b330c2f160fd19a
maskudo <manujkarki101@gmail.com>
2022-07-11 22:33:35 +0545

    add graphics functions

--- include/graphics.hpp
+++ include/graphics.hpp
@@ -1,8 +1,16 @@
 #pragma once
+#include "SDL.h"
 class Graphics {
 private:
-    /* data */
+    SDL_Window *window{};
+    SDL_Renderer *renderer{};
+    SDL_Texture *texture{};
+
 public:
-    Graphics(/* args */);
+    Graphics(char const *title, int windowWidth, int windowHeight, int textureWidth, int textureHeight);
+
     ~Graphics();
+
+    void Update(void const *buffer, int pitch);
+    bool ProcessInput(uint8_t *keys);
 };

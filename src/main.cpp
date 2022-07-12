0f72b6d4c973b3d054b1f92208123e0f1ac27e65
maskudo <manujkarki101@gmail.com>
2022-07-12 21:05:26 +0545

    implement clock cycle and fps

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,9 @@
 #include <iostream>
 int main(int argc, char **argv) {
     const int SCALE = 10;
+    const int FREQ = 500;
+    const int FPS = 60;
+    int delay = 1000 / FREQ;
     Graphics graphics("Chip8", SCALE * 64, SCALE * 32, 64, 32);
     chip8::Chip8 interpreter = chip8::Chip8();
     interpreter.Reset();
@@ -15,14 +18,21 @@ int main(int argc, char **argv) {
     int videoPitch = sizeof(interpreter.display[0]) * 64;
 
     bool quit = false;
-
+    int nextFrame = SDL_GetTicks();
     while (!quit) {
+        int start = SDL_GetTicks();
         quit = graphics.ProcessInput(interpreter.keypad);
 
         interpreter.Tick();
+        if (SDL_GetTicks() >= nextFrame) {
+            graphics.Update(interpreter.display, videoPitch);
+            nextFrame = SDL_GetTicks() + (1000 / FPS);
+            // std::cout << SDL_GetTicks() << std::endl;
+        }
 
-        graphics.Update(interpreter.display, videoPitch);
-        SDL_Delay(2);
+        int totalDelay = SDL_GetTicks() - start;
+        if (totalDelay < delay)
+            SDL_Delay(delay - totalDelay);
     }
 
     return 0;

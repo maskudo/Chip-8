df94777af1a859f5705a581a83b7765274d59bc0
maskudo <manujkarki101@gmail.com>
2022-07-15 19:53:07 +0545

    get scale and clock freq values from terminal args

--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,27 @@
 #include "graphics.hpp"
 #include <iostream>
 int main(int argc, char **argv) {
-    const int SCALE = 10;
-    const int FREQ = 800;
+    if (argc != 4) {
+        std::cerr << "Usage: " << argv[0] << " <ROM> <Scale> <Delay>\n";
+        std::exit(EXIT_FAILURE);
+    }
+
+    const int SCALE = std::stoi(argv[2]);
+    const int FREQ = std::stoi(argv[3]);
     int delay = 1000 / FREQ;
+
     Graphics *graphics = new Graphics("Chip8", SCALE * 64, SCALE * 32, 64, 32);
     chip8::Chip8 interpreter = chip8::Chip8();
+
     interpreter.Reset();
     if (!interpreter.LoadROM(argv[1])) {
         std::cerr << "Unable to load" << argv[1] << std::endl;
         return -1;
     }
+
     // number of bytes in a row
     int videoPitch = sizeof(interpreter.display[0]) * 64;
-
     bool quit = false;
-    int nextFrame = SDL_GetTicks();
     while (!quit) {
         int start = SDL_GetTicks();
         quit = graphics->ProcessInput(interpreter.keypad);
